01748feea41cb83c087be237e36e9840bb5bf81c
xDOASx <benesbit@yahoo.com>
2019-05-13 10:52:24 -0700

    Remove unwanted code

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,7 +3,6 @@ FactoryBot.define do
     name {"Project Runway"}
     # due_date 1.week.from.now
     due_date { Date.today - rand(50) }
-    # slug { "#{name.downcase.gsub(" ", "_")}" }
   end
 
   factory :big_project, class: Project do
