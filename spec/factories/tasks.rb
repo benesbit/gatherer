78eca2146416dd9a119b2dcfa993626e17c21f2b
xDOASx <benesbit@yahoo.com>
2019-05-13 13:45:07 -0700

    Add traits to task factory.

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,13 +6,34 @@ FactoryBot.define do
   # end
   factory :task do
     sequence(:title) { |n| "Task #{n}" }
+    size { 3 }
+    completed_at { nil }
 
-    factory :big_task, parent: :task do
+    trait :small do
+      size 1
+    end
+
+    trait :large do
       size 5
     end
 
-    factory :small_task, parent: :task do
-      size 1
+    trait :soon do
+      due_date { 1.day.from_now }
     end
+
+    trait :later do
+      due_date { 1.month.from_now }
+    end
+
+    trait :newly_complete do
+      completed_at { 1.day.ago }
+    end
+
+    trait :long_complete do
+      completed_at { 6.months.ago }
+    end
+
+    factory :trivial, class: Task, traits: %i[small later]
+    factory :panic, class: Task, traits: %i[large soon]
   end
 end
