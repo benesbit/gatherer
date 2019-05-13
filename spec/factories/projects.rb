62fa4647432ee3fa3595cead92b12c05b529ed3a
xDOASx <benesbit@yahoo.com>
2019-05-13 10:09:04 -0700

    Fix factories by using new implementation with the braces

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :project do
-    name("Project Runway")
+    name {"Project Runway"}
     # due_date 1.week.from.now
     due_date { Date.today - rand(50) }
-    slug { "#{name.downcase.gsub(" ", "_")}" }
+    # slug { "#{name.downcase.gsub(" ", "_")}" }
   end
 
   factory :big_project, class: Project do
-    name("Big Project")
+    name {"Big Project"}
   end
 end
