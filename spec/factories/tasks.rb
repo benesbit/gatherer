7720ba3a9607dec7c0eea431408480a79e87ba14
xDOASx <benesbit@yahoo.com>
2019-05-13 11:42:23 -0700

    Add parent and nested child factories to task

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,7 +1,18 @@
 FactoryBot.define do
+  # factory :task do
+  #   title { "Thing to do" }
+  #   size { 1 }
+  #   completed_at { nil }
+  # end
   factory :task do
-    title {"Thing to do"}
-    size {1}
-    completed_at {nil}
+    sequence(:title) { |n| "Task #{n}" }
+
+    factory :big_task, parent: :task do
+      size 5
+    end
+
+    factory :small_task, parent: :task do
+      size 1
+    end
   end
 end
