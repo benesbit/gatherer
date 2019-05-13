de545bf56a5599372401d32f435eab90ffbf050d
xDOASx <benesbit@yahoo.com>
2019-05-12 19:36:18 -0700

    Add slug using previously assigned value

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     name "Project Runway"
     # due_date 1.week.from.now
     due_date { Date.today - rand(50) }
+    slug { "#{name.downcase.gsub(" ", "_")}" }
   end
 
   factory :big_project, class: Project do
