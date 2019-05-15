21b2947c90ba028a777a48c3222e71bed5fd639b
xDOASx <benesbit@yahoo.com>
2019-05-14 18:44:39 -0700

    Remove unwanted code

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,9 +1,4 @@
 FactoryBot.define do
-  # factory :task do
-  #   title { "Thing to do" }
-  #   size { 1 }
-  #   completed_at { nil }
-  # end
   factory :task do
     sequence(:title) { |n| "Task #{n}" }
     size { 3 }
